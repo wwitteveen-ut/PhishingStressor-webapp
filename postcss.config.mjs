295c5bebbeead5af591500f681cb89bac9fbc8b2
Wessel Witteveen <Witteveen.w-1@student.utwente.nl>
2025-05-14 12:16:13 +0200

    set up mantine

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,17 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
+    },
+    '@tailwindcss/postcss': {}, // Tailwind plugin as object, not just string
+  },
 };
 
 export default config;
