9f430068a2055f147fd1066b5453d071b5b74bba
Wessel Witteveen <Witteveen.w-1@student.utwente.nl>
2025-06-03 11:31:03 +0200

    styling stuff

--- src/mail/components/EmailListItem/EmailListItem.tsx
+++ src/mail/components/EmailListItem/EmailListItem.tsx
@@ -1,53 +1,66 @@
 import { useEmailClientStore } from '@/mail/providers/EmailClientStoreProvider';
 import { ZustandEmail } from '@/mail/store/types';
-import { PaperclipIcon } from 'lucide-react';
+import { Group, Box, Text, ThemeIcon, Tooltip } from '@mantine/core';
+import { Paperclip } from 'lucide-react';
+import classes from './EmailListItem.module.css';
+
 
 
 export default function EmailListItem({email, isSelected}: {email:ZustandEmail, isSelected: boolean}) {
   const selectEmailId = useEmailClientStore((state) => state.selectEmailId);
 
   return (
-      <div
-        className={`p-3 border-b border-gray-200 cursor-pointer ${isSelected ? 'bg-blue-50' : email.isRead ? 'bg-white' : 'bg-gray-50'} hover:bg-gray-100`}
-        onClick={() => selectEmailId(email.id)}
+    <Box
+      onClick={() => selectEmailId(email.id)}
+      p="md"
+      className={`${classes.mailItem} ${!email.isRead ? classes.unread : ''} ${isSelected ? classes.active : ''}`}
+    >
+      <Group justify="space-between">
+      <Box style={{ flex: 1, minWidth: 0 }}>
+        <Tooltip openDelay={500} inline offset={4} label={email.senderAddress}>
+          <Text
+            size="sm"
+            fw={email.isRead ? 400 : 600}
+            c={email.isRead ? 'gray.7' : 'gray.9'}
+            truncate
+            >
+            {email.senderName}
+          </Text>
+        </Tooltip>
+        
+      </Box>
+      <Text size="xs" c="gray.6">
+        {new Date(email.sendAt).toLocaleString([], {
+          weekday: 'short',
+          month: 'short',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+        })}
+      </Text>
+      </Group>
+      <Box>
+      <Text
+        size="sm"
+        fw={600}
+        c={"blue.5"}
+        truncate
       >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="flex-1 min-w-0">
-              <p
-                className={`text-sm truncate`}
-              >
-                {email.senderName}
-              </p>
-            </div>
-          </div>
-          <div className="text-xs text-gray-500">
-            {new Date(email.sendAt).toLocaleString([], {
-              weekday: 'short',
-              month: 'short',
-              day: 'numeric',
-              hour: '2-digit',
-              minute: '2-digit',
-            })}
-          </div>
-        </div>
-        <div className="mt-1">
-          <p
-            className={`text-sm truncate`}
-          >
-            {email.title}
-          </p>
-          <div className="flex items-center mt-1">
-            <p className="text-xs text-gray-500 truncate flex-1">
-              {email.content}
-            </p>
-            <div className="flex items-center ml-2">
-              {email.attachments.length > 0 && (
-                <PaperclipIcon size={14} className="text-gray-400" />
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
+        {email.title}
+      </Text>
+      <Group justify="space-between">
+        <Text size="xs" c="gray.6" truncate style={{ flex: 1 }}>
+        {email.content}
+        </Text>
+        <Group gap="xs">
+        {email.attachments.length > 0 && (
+          <ThemeIcon color='gray.6' variant='transparent'>
+            <Paperclip size={14} />
+          </ThemeIcon>
+        ) }
+        </Group>
+      </Group>
+      </Box>
+      </Box>
   )
 }
