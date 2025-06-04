c6c1c0a379b20e62579056072ebfcf7164c7b215
Wessel Witteveen <Witteveen.w-1@student.utwente.nl>
2025-06-04 14:38:51 +0200

    small preparation for the events

--- src/mail/store/types.ts
+++ src/mail/store/types.ts
@@ -1,3 +1,5 @@
+import { DataPoint } from 'heatmap-ts';
+
 export interface EmailAttachmentData {
   filename: string;
   id: string;
@@ -23,4 +25,19 @@ export interface EmailProperties {
 
 export interface ZustandEmail extends Email, EmailProperties {
   sendAt: Date;
+}
+
+export enum UserEventType {
+  TIME_OPENED = "TIME_OPENED",
+  TIME_CLOSED = "TIME_CLOSED",
+  ATTACHMENT_OPENED = "ATTACHMENT_OPENED",
+  ATTACHMENT_CLOSED = "ATTACHMENT_CLOSED",
+  HEATMAP = "HEATMAP",
+  OTHER = "OTHER"
+}
+
+export interface UserEvent {
+  type: UserEventType;
+  timestamp: string;
+  extra?: DataPoint[];
 }
